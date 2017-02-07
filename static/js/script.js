1745963cb25908c2ed8065ba05548ed9616c108b
Aman Deep <amandeep1024@gmail.com>
2017-02-08 03:23:14 +0530

    changes

--- static/js/script.js
+++ static/js/script.js
@@ -41,7 +41,7 @@ function validate_signup_form(){
 		message2 += 'no mobile given\n';
 	}
 
-	else if (/^\d{10}$/.test(document.myform2.mobile.value) == false){
+	else if (document.myform2.mobile.value.length != 10){
 		message2 += "invalid phone no."
 	}
 
@@ -66,6 +66,53 @@ function validate_signup_form(){
 	}
 
 	if(message2 == ""){
+		alert("signup Successful");
+		return true;
+	}
+
+	else{
+		alert(message2);
+		return false;
+	}
+
+}
+
+
+function validate_signup_teacher_form(){
+
+	if(document.myform2.name.value == ""){
+		message2 += 'No username given\n';
+	}
+
+
+	if(document.myform2.gender.value == ""){
+		message2 += 'no gender given\n';
+	}
+	if(document.myform2.mobile.value == ""){
+		message2 += 'no mobile given\n';
+	}
+
+	else if (document.myform2.mobile.value.length != 10){
+		message2 += "invalid phone no."
+	}
+
+	if(document.myform2.password.value == ""){
+		message2 += 'no password given\n';
+	}
+	if(document.myform2.confirm_password.value == ""){
+		message2 += 'no password confirmation given\n';
+	}
+
+	if(document.myform2.programme.value == 'blank'){
+		message2 += 'no programme given\n';
+	}
+
+	if(document.myform2.semester.value == 'blank'){
+		message2 += 'no semester given\n';
+	}
+
+	if(message2 == ""){
+		alert("signup Successful");
 		return true;
 	}
 
